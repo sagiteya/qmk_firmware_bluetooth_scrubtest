a635d39e41fe4d7188e68cdb3d98d4f4e7ae337b
sagiteya <118155772+sagiteya@users.noreply.github.com>
2023-02-03 01:50:11 +0900

    macros to  k6 pro

--- keyboards/keychron/k6_pro/ansi/rgb/keymaps/custom/keymap.c
+++ keyboards/keychron/k6_pro/ansi/rgb/keymaps/custom/keymap.c
@@ -23,7 +23,10 @@ enum layers{
   MAC_FN1,
   WIN_FN1,
   FN2,
-  FN3
+  FN3,
+  FN4,
+  FN5,
+  FN6
 };
 
 bool is_alt_tab_active = false; // ADD this near the beginning of keymap.c
@@ -31,6 +34,9 @@ uint16_t alt_tab_timer = 0;     // we will be using them soon.
 
 enum custom_keycodes {          // Make sure have the awesome keycode ready
   ALT_TAB = SAFE_RANGE,
+  GRABURL,
+  REFRESH,
+
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -45,6 +51,20 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         register_code(KC_TAB);
       } else {
         unregister_code(KC_TAB);
+      }
+      break;
+    case GRABURL:
+      if (record->event.pressed) {
+        SEND_STRING(SS_TAP(X_F6) SS_LCTL("x") SS_TAP(X_F6) SS_TAP(X_F6) SS_TAP(X_F6));
+      } else {
+
+      }
+      break;
+    case REFRESH:
+      if (record->event.pressed) {
+        SEND_STRING(SS_TAP(X_F6) SS_LCTL("V") SS_TAP(X_ENTER));
+      } else {
+
       }
       break;
   }
@@ -82,6 +102,26 @@ bool get_ignore_mod_tap_interrupt(uint16_t keycode, keyrecord_t *record) {
             // Do not force the mod-tap key press to be handled as a modifier
             // if any other key was pressed while the mod-tap key is held down.
             return true;
+        case CTL_T(KC_UP):
+            // Do not force the mod-tap key press to be handled as a modifier
+            // if any other key was pressed while the mod-tap key is held down.
+            return true;
+        case CTL_T(KC_DOWN):
+            // Do not force the mod-tap key press to be handled as a modifier
+            // if any other key was pressed while the mod-tap key is held down.
+            return true;
+        case SFT_T(KC_LEFT):
+            // Do not force the mod-tap key press to be handled as a modifier
+            // if any other key was pressed while the mod-tap key is held down.
+            return true;
+        case SFT_T(KC_RIGHT):
+            // Do not force the mod-tap key press to be handled as a modifier
+            // if any other key was pressed while the mod-tap key is held down.
+            return true;
+        //case SFT_T(KC_RIGHT):
+            // Do not force the mod-tap key press to be handled as a modifier
+            // if any other key was pressed while the mod-tap key is held down.
+          //  return true;
         default:
             // Force the mod-tap key press to be handled as a modifier if any
             // other key was pressed while the mod-tap key is held down.
@@ -89,42 +129,43 @@ bool get_ignore_mod_tap_interrupt(uint16_t keycode, keyrecord_t *record) {
     }
 }
 
-const uint16_t PROGMEM test_combo1[] = {KC_K, KC_L, COMBO_END}; // K + L = Single Quote
-const uint16_t PROGMEM test_combo2[] = {KC_D, KC_V, COMBO_END}; // D + V = Enter
-const uint16_t PROGMEM test_combo3[] = {SFT_T(KC_F), KC_D, COMBO_END};  // F + D = Delete
-const uint16_t PROGMEM test_combo4[] = {SFT_T(KC_J), KC_L, COMBO_END};  // J + L = Double Quote
-const uint16_t PROGMEM test_combo5[] = {KC_U, KC_I, COMBO_END};  // U + I = Underscore
-const uint16_t PROGMEM test_combo6[] = {KC_I, KC_O, COMBO_END}; // I + O = Minus
+const uint16_t PROGMEM test_combo1[] = {SFT_T(KC_F), SFT_T(KC_J), COMBO_END}; // F + J = Single Quote
+const uint16_t PROGMEM test_combo2[] = {KC_D, KC_C, COMBO_END}; // D + C = Enter
+//const uint16_t PROGMEM test_combo3[] = {KC_D, KC_S, COMBO_END};  // D + S = Delete
+const uint16_t PROGMEM test_combo4[] = {KC_M, KC_COMM, COMBO_END};  // J + L = Double Quote
+const uint16_t PROGMEM test_combo5[] = {KC_I, SFT_T(KC_J), COMBO_END};  // I + J = Underscore
+const uint16_t PROGMEM test_combo6[] = {KC_O, SFT_T(KC_J), COMBO_END}; // O + J = Minus
 const uint16_t PROGMEM test_combo7[] = {KC_S, KC_D, COMBO_END}; // S + D = Backspace
-const uint16_t PROGMEM test_combo8[] = {KC_BSLS, KC_DELETE, COMBO_END}; // Backslash + Del = PrintScreen
-const uint16_t PROGMEM test_combo9[] = {SFT_T(KC_J), KC_K, COMBO_END}; // K + J = Pipe
-const uint16_t PROGMEM test_combo10[] = {CTL_T(KC_Z), KC_X, COMBO_END}; // K + L = ctrl a
-const uint16_t PROGMEM test_combo11[] = {KC_X, KC_C, COMBO_END}; // K + L = ctrl x
-const uint16_t PROGMEM test_combo12[] = {KC_C, KC_V, COMBO_END}; // K + L = ctrl v
-const uint16_t PROGMEM test_combo13[] = {KC_W, SFT_T(KC_F), COMBO_END}; // W + F = Grave (lang swap) DOES NOT WORK
-const uint16_t PROGMEM test_combo14[] = {KC_S, SFT_T(KC_F), COMBO_END}; // K + L = Ctrl M
-const uint16_t PROGMEM test_combo15[] = {KC_3, KC_4, COMBO_END}; // K + L = alt left 
-const uint16_t PROGMEM test_combo16[] = {KC_F3, KC_F4, COMBO_END}; // K + L = alt left DOES NOT WORK
-const uint16_t PROGMEM test_combo17[] = {KC_4, KC_2, COMBO_END}; // K + L = alt right
-const uint16_t PROGMEM test_combo18[] = {KC_F4, KC_F2, COMBO_END}; // K + L = alt right
-const uint16_t PROGMEM test_combo19[] = {KC_8, KC_9, COMBO_END}; // K + L = left (
-const uint16_t PROGMEM test_combo20[] = {KC_9, KC_0, COMBO_END}; // K + L = right )
-const uint16_t PROGMEM test_combo21[] = {KC_BSPC, KC_RBRC, COMBO_END}; // K + L = Delete
-const uint16_t PROGMEM test_combo22[] = {KC_W, KC_Q, COMBO_END}; // K + L = Home
-const uint16_t PROGMEM test_combo23[] = {KC_Q, KC_S, COMBO_END}; // K + L = End
+const uint16_t PROGMEM test_combo8[] = {KC_LBRC, KC_BSLS, COMBO_END}; // Backslash + Del = PrintScreen
+const uint16_t PROGMEM test_combo9[] = {KC_K, SFT_T(KC_J), COMBO_END}; // K + J = Pipe
+const uint16_t PROGMEM test_combo10[] = {LT(FN5,KC_L), KC_K, COMBO_END}; // F + D = Left
+const uint16_t PROGMEM test_combo11[] = {KC_X, KC_C, COMBO_END}; // X + C = Tab
+const uint16_t PROGMEM test_combo12[] = {KC_X, KC_V, COMBO_END}; // X + V = Shift Tab
+const uint16_t PROGMEM test_combo13[] = {KC_W, SFT_T(KC_F), COMBO_END}; // W + F = Grave (lang swap)
+const uint16_t PROGMEM test_combo14[] = {KC_S, SFT_T(KC_F), COMBO_END}; // S + F = Ctrl M
+const uint16_t PROGMEM test_combo15[] = {KC_3, KC_4, COMBO_END}; // 3 + 4 = alt left 
+const uint16_t PROGMEM test_combo16[] = {KC_F3, KC_F4, COMBO_END}; // F3 + F4 = alt left DOES NOT WORK
+const uint16_t PROGMEM test_combo17[] = {KC_4, KC_3, COMBO_END}; // 4 + 2 = alt right
+const uint16_t PROGMEM test_combo18[] = {KC_F4, KC_F3, COMBO_END}; // F4 + F2 = alt right
+const uint16_t PROGMEM test_combo19[] = {KC_9, KC_8, COMBO_END}; // 8 + 9 = left (
+const uint16_t PROGMEM test_combo20[] = {KC_0, KC_9, COMBO_END}; // 9 + 0 = right )
+const uint16_t PROGMEM test_combo21[] = {KC_K, KC_M, COMBO_END}; // Back + Lbracket(JP) = Delete
+const uint16_t PROGMEM test_combo22[] = {KC_R, KC_Q, COMBO_END}; // R + Q = Home
+const uint16_t PROGMEM test_combo23[] = {KC_T, KC_W, COMBO_END}; // Q + R = End
+const uint16_t PROGMEM test_combo24[] = {KC_COMM, KC_DOT, COMBO_END};  // D + F = Right
 combo_t key_combos[COMBO_COUNT] = {
     COMBO(test_combo1, LSFT(KC_7)),
     COMBO(test_combo2, KC_ENTER), // keycodes with modifiers are possible too!
-    COMBO(test_combo3, KC_DELETE),
-    COMBO(test_combo4, LSFT(KC_2)),
+    //COMBO(test_combo3, KC_DELETE),
+    COMBO(test_combo4, KC_DELETE),
     COMBO(test_combo5, LSFT(KC_INT1)),
     COMBO(test_combo6, KC_MINUS),
     COMBO(test_combo7, KC_BACKSPACE),
     COMBO(test_combo8, KC_PRINT_SCREEN),
     COMBO(test_combo9, LSFT(KC_INT3)),
-    COMBO(test_combo10, LCTL(KC_A)),
-    COMBO(test_combo11, LCTL(KC_X)),
-    COMBO(test_combo12, LCTL(KC_V)),
+    COMBO(test_combo10, KC_BSPC),
+    COMBO(test_combo11, KC_TAB),
+    COMBO(test_combo12, LSFT(KC_TAB)),
     COMBO(test_combo13, KC_GRV),
     COMBO(test_combo14, LCTL(KC_M)),
     COMBO(test_combo15, LALT(KC_LEFT)),
@@ -133,9 +174,10 @@ combo_t key_combos[COMBO_COUNT] = {
     COMBO(test_combo18, LALT(KC_RIGHT)),
     COMBO(test_combo19, LSFT(KC_8)),
     COMBO(test_combo20, LSFT(KC_9)),
-    COMBO(test_combo21, KC_DELETE),
+    COMBO(test_combo21, KC_ENT),
     COMBO(test_combo22, KC_HOME),
-    COMBO(test_combo23, KC_END)
+    COMBO(test_combo23, KC_END),
+    COMBO(test_combo24, S(KC_7))
 };
 
 //void pointing_device_init_user(void) {
@@ -146,18 +188,18 @@ combo_t key_combos[COMBO_COUNT] = {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [MAC_BASE] = LAYOUT_ansi_68(
-     KC_ESC,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSLS, KC_DELETE,
-     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSPC, KC_HOME,
-     CTL_T(KC_GRV),  KC_A,     KC_S,     KC_D,     SFT_T(KC_F),     KC_G,     KC_H,     SFT_T(KC_J),     KC_K,     KC_L,     CTL_T(KC_SCLN),  KC_QUOT,            KC_ENT,  KC_END,
-     OSM(MOD_LSFT),  CTL_T(KC_Z),     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  MT(MOD_RSFT,KC_INT1),  KC_UP,    KC_PGDN,
+     KC_ESC,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_LBRC, KC_HOME,
+     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_RBRC,  KC_BSLS,  KC_BSPC, KC_DEL,
+     CTL_T(KC_GRV),  LT(FN3,KC_A),     KC_S,     KC_D,     SFT_T(KC_F),     KC_G,     KC_H,     SFT_T(KC_J),     KC_K,     LT(FN5,KC_L),     CTL_T(KC_SCLN),  KC_QUOT,            LT(FN2,KC_ENT),  KC_END,
+     MT(MOD_LSFT,KC_BSPC),  CTL_T(KC_Z),     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  MT(MOD_RSFT,KC_INT1),  KC_UP,    KC_PGDN,
      TG(1),  KC_LGUI, KC_LALT,                               LT(WIN_BASE,KC_SPC),                       KC_RALT,MO(FN2),KC_RCTL, KC_LEFT,  KC_DOWN, KC_RGHT),
 
 [WIN_BASE] = LAYOUT_ansi_68(
-     KC_TRNS,  MACRO00,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_TRNS,  KC_TRNS,
-     C(S(KC_TAB)),  C(KC_TAB),  KC_MS_BTN2,  KC_MS_UP,  KC_MS_BTN1,  KC_MS_WH_UP,  KC_TRNS,  KC_KP_7,  KC_KP_8,  KC_KP_9,  KC_KP_MINUS,  MACRO02,  MACRO01,  C(KC_BSPC),  KC_PGUP,
-     KC_TRNS,  KC_TRNS,  KC_MS_LEFT,  KC_MS_DOWN,  KC_MS_RIGHT,  KC_MS_WH_DOWN,  KC_TRNS,  KC_KP_4,  KC_KP_5,  KC_KP_6,  KC_KP_PLUS,  KC_KP_ASTERISK,            KC_TRNS,  KC_PGDN,
+     GRABURL,  REFRESH,    KC_F2,    A(KC_LEFT),    A(KC_RIGHT),    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_TRNS,  KC_TRNS,
+     C(S(KC_TAB)),  C(KC_TAB),  KC_MS_BTN2,  KC_MS_UP,  KC_MS_BTN1,  KC_MS_WH_UP,  KC_NUM,  KC_KP_7,  KC_KP_8,  KC_KP_9,  KC_KP_MINUS,  LSFT(KC_8),  LSFT(KC_9),  KC_TRNS,  KC_PGUP,
+     KC_TRNS,  KC_BSPC,  KC_MS_LEFT,  KC_MS_DOWN,  KC_MS_RIGHT,  KC_MS_WH_DOWN,  KC_BSPC,  KC_KP_4,  KC_KP_5,  KC_KP_6,  KC_KP_PLUS,  KC_KP_ASTERISK,            KC_TRNS,  KC_PGDN,
      KC_TRNS,  CTL_T(KC_LEFT),  KC_DOWN,  KC_UP,  KC_RIGHT,  KC_TRNS,  KC_KP_0,  KC_KP_1,  KC_KP_2,  KC_KP_3,  KC_KP_DOT,  KC_MS_BTN1,  KC_MS_UP,  KC_MS_BTN2,
-     KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                      KC_TRNS,  KC_MS_WH_UP,  KC_MS_WH_DOWN,  KC_MS_LEFT,  KC_MS_DOWN,  KC_MS_RIGHT),
+     KC_TRNS,  KC_ENTER,  KC_TRNS,                                KC_TRNS,                      KC_TRNS,  KC_MS_WH_UP,  KC_MS_WH_DOWN,  KC_MS_LEFT,  KC_MS_DOWN,  KC_MS_RIGHT),
 
 [MAC_FN1] = LAYOUT_ansi_68(
      KC_GRV,   KC_BRID,  KC_BRIU,  KC_MCTL,  KC_LNPD,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_TRNS,  RGB_TOG,
@@ -175,15 +217,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [FN2] = LAYOUT_ansi_68(
      KC_TRNS,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_TRNS,  KC_TRNS,
-     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
-     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,  KC_TRNS,
-     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  BAT_LVL,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_UP,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_LEFT,  KC_RIGHT,            KC_TRNS,  KC_TRNS,
+     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  BAT_LVL,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_DOWN,  KC_TRNS,  KC_TRNS,  KC_TRNS,
      KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS),
 [FN3] = LAYOUT_ansi_68(
      KC_TRNS,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_TRNS,  KC_TRNS,
-     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
-     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,  KC_TRNS,
-     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  BAT_LVL,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+     KC_TRNS,  KC_TRNS,  KC_HOME,  MT(MOD_LCTL,KC_UP),  KC_END,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+     KC_TRNS,  KC_TRNS,  MT(MOD_LSFT,KC_LEFT),  MT(MOD_LCTL,KC_DOWN),  MT(MOD_LSFT,KC_RIGHT),  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,  KC_TRNS,
+     KC_TRNS,  KC_TRNS,  C(KC_X),  C(KC_C),  C(KC_V),  BAT_LVL,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+     KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS),
+[FN4] = LAYOUT_ansi_68(
+     KC_TRNS,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_TRNS,  KC_TRNS,
+     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,
+     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,             KC_TRNS,  KC_TRNS,
+     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  BAT_LVL,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,
+     KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS),
+[FN5] = LAYOUT_ansi_68(
+     KC_TRNS,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_TRNS,  KC_TRNS,
+     ALT_TAB,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TAB,  KC_TRNS,  LSFT(KC_7),  KC_UP,  KC_TRNS,  KC_HOME,  KC_END,  KC_TRNS,   KC_TRNS,  KC_TRNS,
+     KC_TRNS,  LCTL(KC_A),  KC_LALT,  KC_LCTL,  KC_LSFT,  KC_TRNS,  KC_TRNS,  KC_LEFT,  KC_RIGHT,  KC_TRNS,  KC_TRNS,  KC_TRNS,             KC_TRNS,  KC_TRNS,
+     KC_TRNS,  LCTL(KC_Z), LCTL(KC_X), LCTL(KC_C), LCTL(KC_V),  BAT_LVL,  KC_TRNS,  KC_DOWN,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,
+     KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS),
+[FN6] = LAYOUT_ansi_68(
+     KC_TRNS,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_TRNS,  KC_TRNS,
+     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,
+     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,             KC_TRNS,  KC_TRNS,
+     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  BAT_LVL,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,
      KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS),
 };
-
